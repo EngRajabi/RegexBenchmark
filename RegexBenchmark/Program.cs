cd8d6157aad022d0d26d78d567756dfff6cd3642
Mohsen Rajabi <m.kabir8895@gmail.com>
2022-03-17 14:15:32 +0330

    init

---
+++ RegexBenchmark/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Perfolizer.Horology;
+
+namespace RegexBenchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    var t = new RegexBenchmark();
+            //    t.RegexStaticMethod();
+            //    t.RegexStaticVariable();
+
+            RunBenchmarks();
+        }
+
+        private static void RunBenchmarks()
+        {
+            Regex.CacheSize += 100;
+            var config = ManualConfig.Create(DefaultConfig.Instance)
+                .AddAnalyser(BenchmarkDotNet.Analysers.EnvironmentAnalyser.Default)
+                .AddExporter(BenchmarkDotNet.Exporters.MarkdownExporter.GitHub)
+                .AddDiagnoser(BenchmarkDotNet.Diagnosers.MemoryDiagnoser.Default)
+                .AddColumn(StatisticColumn.AllStatistics)
+                .AddColumn(StatisticColumn.Median)
+                .AddColumn(StatisticColumn.StdDev)
+                .AddColumn(StatisticColumn.StdErr)
+                .AddColumn(StatisticColumn.OperationsPerSecond)
+                .AddColumn(BaselineRatioColumn.RatioMean)
+                .AddColumn(RankColumn.Arabic)
+                .AddJob(Job.Default.WithRuntime(CoreRuntime.Core60)
+                    .WithIterationCount(32)
+                    .WithInvocationCount(64)
+                    .WithIterationTime(TimeInterval.FromSeconds(120))
+                    .WithWarmupCount(6)
+                    .WithLaunchCount(1));
+            BenchmarkRunner.Run<RegexBenchmarkStep2>(config);
+        }
+    }
+    
+}
---
+++ RegexBenchmark/RegexBenchmark.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using BenchmarkDotNet.Attributes;
+
+namespace RegexBenchmark
+{
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest, BenchmarkDotNet.Order.MethodOrderPolicy.Declared)]
+    public class RegexBenchmarkStep1
+    {
+        private const string Pattern = @"\d+";
+        private const string Text = "asdjkla j;ajkd;da da asdkswegredsgjvnxvn p ajk;d k;jajkdakjdn k;jbn ak;bjdnak;jbnd 123 ak;bjd ak;sbjd ask;db ;";
+
+        private static readonly Regex Regex = new(Pattern);
+
+        //OriginalCode
+        [Benchmark]
+        public void OriginalCode()
+        {
+            var regex = new Regex(Pattern);
+            var f = regex.IsMatch(Text);
+        }
+
+        //Case1
+        [Benchmark]
+        public void RegexStaticMethodCase1()
+        {
+            var f = Regex.IsMatch(Text, Pattern);
+        }
+
+        //Case2
+        [Benchmark]
+        public void RegexStaticVariableCase2()
+        {
+            var f = Regex.IsMatch(Text);
+        }
+    }
+
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest, BenchmarkDotNet.Order.MethodOrderPolicy.Declared)]
+    public class RegexBenchmarkStep2
+    {
+        private const string Pattern = @"\d+";
+        private const string Text = "asdjkla j;ajkd;da da asdkswegredsgjvnxvn p ajk;d k;jajkdakjdn k;jbn ak;bjdnak;jbnd 123 ak;bjd ak;sbjd ask;db ;";
+
+        private static readonly RegexOptions regexOptions = RegexOptions.Compiled;
+        private static readonly Regex Regex = new(Pattern, regexOptions, TimeSpan.FromMilliseconds(50));
+
+        //OriginalCode
+        [Benchmark]
+        public void OriginalCode()
+        {
+            var regex = new Regex(Pattern, regexOptions, TimeSpan.FromMilliseconds(50));
+            var f = regex.IsMatch(Text);
+        }
+
+        //Case1
+        [Benchmark]
+        public void RegexStaticMethodCase1()
+        {
+            var f = Regex.IsMatch(Text, Pattern, regexOptions, TimeSpan.FromMilliseconds(50));
+        }
+
+        //Case2
+        [Benchmark]
+        public void RegexStaticVariableCase2()
+        {
+            var f = Regex.IsMatch(Text);
+        }
+    }
+}
